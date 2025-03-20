ddc593549196252ee04c7c60584a862225fb04e0
Samarveer15 <Deep.pune@gmail.com>
2025-03-20 19:50:19 +0530

    Project

--- src/index.js
+++ src/index.js
@@ -1,74 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-//first activity -4
-/*class Customer extends React.Component{
-  render(){
-    return <h2> I am from {this.props.city
-  }</h2>;
-}
-}
-class Details extends React.Component{
-  render(){
-    return (
-      <div>
-        <h1>hello</h1>
-        <Customer city="Chennai"/>
-        </div>
-    )
-  }
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Details />);*/
-// Second Activity
-/*class Customer extends React.Component{
-  render(){
-    return <h2> I am from {this.props.city
-  }</h2>;
-}
-}
-class Details extends React.Component{
-  render(){
-    const cityname= "AMIRTSAR";
-    return (
-      <div>
-        <h1>hello</h1>
-        <Customer city= {cityname}/>
-        </div>
-    )
-  }
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Details />);*/
-
-// Third Activity
-/*class Customer extends React.Component{
-  render(){
-    return <h2> I am from {this.props.customerDetails.country
-  }</h2>;
-}
-}
-class Details extends React.Component{
-  render(){
-    const customerinfo= {city: 'Banglore', country:'India'};
-    return (
-      <div>
-        <h1>hello</h1>
-        <Customer customerDetails= {customerinfo}/>
-        </div>
-    )
-  }
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Details />);*/
-
-//fourth activity 
+import ReactDOM from 'react-dom/client'; 
 class Customer extends React.Component{
  constructor(props){
     super (props);
-    this.info = {location:"Tamil Nadu"};
+    this.info = {location:"Uttar Pradesh"};
   }
   render(){
     return <h2> I am from {this.props.city},{this.info.location}!</h2>;
@@ -79,8 +14,8 @@ class Details extends React.Component{
   render(){
     return (
       <div>
-        <h1>hello</h1>
-        <Customer city="Chennai"/>
+        <h1>Hello</h1>
+        <Customer city="Noida"/>
         </div>
     );
   }
